9bfdaef47fcdede5b6faba2372a8d0e92284c370
SitiKomalasari14 <sitikomalasari1417@gmail.com>
2022-10-13 17:14:43 +0700

    menyelesaikan video 6 ASSETS

--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun. Saya seorang <?= $data['pekerjaan']; ?>.</p>
+<div class="container">
+  <h1>About Me</h1>
+  <img src="<?= BASEURL; ?>/img/m.jpg" alt="mala" width="200" class="rounded-circle shadow">
+  <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun. Saya seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -1,1 +1,8 @@
-<h1>Selamat datang di Website saya</h1>
+<div class="container">
+  <div class="jumbotron jumbotron-fluid mt-5">
+    <div class="container">
+      <h1 class="display-4">Hai!</h1>
+      <p class="lead">Selamat Datang di Website Saya</p>
+    </div>
+  </div>
+</div>
