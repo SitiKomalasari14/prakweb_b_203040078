461927d48284779d7443ce0214d97dd29cd79ba0
SitiKomalasari14 <sitikomalasari1417@gmail.com>
2022-10-13 15:30:08 +0700

    menyelesaikan video 5 VIEW

--- mvc/app/controllers/About.php
+++ mvc/app/controllers/About.php
@@ -1,15 +1,25 @@
 <?php
 
-class About
+class About extends Controller
 {
-  public function index($nama = 'Mala', $pekerjaan = 'Mahasiswa')
+  public function index($nama = 'Mala', $pekerjaan = 'Mahasiswa ', $umur = 19)
   {
-    echo "Halo, Nama saya $nama, saya adalah seorang $pekerjaan";
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+
+    $data['judul'] = 'About Me';
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/footer');
   }
 
 
   public function page()
   {
-    echo 'About/page';
+    $data['judul'] = 'Pages';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
