41b190a52a910f4516ebdc829a9a81ed9faab29d
SitiKomalasari14 <sitikomalasari1417@gmail.com>
2022-10-13 19:53:28 +0700

    menyelesaikan DATA WRAPPER

---
+++ mvc/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('BASEURL', 'http://localhost/prakweb_b_203040078/mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
